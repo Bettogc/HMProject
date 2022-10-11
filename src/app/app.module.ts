882761ead3e9dda77b6b643ee80495c342efbe0b
Erick Gonzalez <egonzalez@indyknit.local>
2022-10-11 16:28:56 -0600

    change primary view not oper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { TicketsAndonComponent } from './tickets-andon/tickets-andon.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
     TicketsAndonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
+  //bootstrap: [TicketsAndonComponent]
 })
 export class AppModule { }
